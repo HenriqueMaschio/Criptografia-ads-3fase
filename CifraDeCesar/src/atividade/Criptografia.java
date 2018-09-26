6bb8a868b52a04e1179cf7834022f7e97ce7761c
Aluno <Aluno@S207M09>
2018-09-25 21:18:31 -0300

    Criptografia

---
+++ CifraDeCesar/src/atividade/Criptografia.java
@@ -0,0 +1,38 @@
+package atividade;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Criptografia {
+
+	public static void main(String[] args) throws IOException {
+		
+//  File f = new File(D:Aluno//Henrique//Teste.txt)	
+		
+		System.out.println("Digite um testo");
+		Scanner teclado = new Scanner(System.in);
+		
+		String textoUsuario = teclado.nextLine();
+		String textoConvertido = textoUsuario.toUpperCase();
+		
+		char[] arrayChar = textoConvertido.toCharArray();
+		String textoCriptografado = "";
+		
+		for(int i = 0; i<arrayChar.length; i++) {
+			arrayChar[i] = (char) (arrayChar[i] -10);
+			textoCriptografado += arrayChar[i];
+		}
+		
+	
+		File f = new File("D:\\Aluno\\Henrique\\Teste.txt");
+		FileOutputStream fout = new FileOutputStream(f);
+		DataOutputStream dout = new DataOutputStream(fout);
+		
+		dout.writeBytes(textoCriptografado);
+	}
+
+}
+
